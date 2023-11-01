588938a3f1c3da255d6c1c3b0b030335daa82094
Rex Wang <cping.wang@gmail.com>
2023-11-01 23:29:16 +0800

    Update func.py

--- func.py
+++ func.py
@@ -3,3 +3,6 @@ from typing import Any
 def main(req: Any):
     # Your function implementation goes here
     return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
